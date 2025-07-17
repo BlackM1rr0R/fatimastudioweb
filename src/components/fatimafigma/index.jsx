ddef0bb8775afb6b25b40b76c13c568ec3e2a15c
BlackM1Rr0R <abdllyevv@gmail.com>
2025-07-17 14:24:30 +0200

    Finish.

--- src/components/fatimafigma/index.jsx
+++ src/components/fatimafigma/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./index.module.css";
-import FatimaPhoto from "../../assets/images/image 12.png";
-import FigmaPhoto from "../../assets/images/image11.png";
+import FatimaPhoto from "../../assets/images/image111.png";
+import FigmaPhoto from "../../assets/images/figmaaaaa.png";
 const FatimaFigma = () => {
   return (
     <div className={styles.background}>
