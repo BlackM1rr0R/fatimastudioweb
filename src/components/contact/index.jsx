33404bd5f8ac5eaf7ffc62551dadd2ca89283b8e
BlackM1rr0R <abdllyevv@gmail.com>
2025-12-27 15:01:48 +0400

    ADD CONTACT SETTINGS

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import styles from "./index.module.css";
+import emailjs from "emailjs-com";
 const Contact = () => {
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_r3zarui",
+      "template_9by6o6s",
+      e.target,
+      "i2BXGjjMpZgU45TJr"
+    )
+    .then(
+      () => {
+        alert("Mesaj uğurla göndərildi ✅");
+        e.target.reset();
+      },
+      (error) => {
+        alert("Xəta baş verdi ❌");
+        console.log(error.text);
+      }
+    );
+  };
   return (
     <div className={styles.background}>
       <div className={styles.controlSide}>
@@ -17,27 +39,13 @@ const Contact = () => {
             </p>
             <h2>Are you ready?</h2>
           </div>
-          <form className={styles.inputs}>
-            <input placeholder="Your name*" type="text" name="name" required />
-            <input
-              placeholder="Your email*"
-              type="email"
-              name="email"
-              required
-            />
-            <input
-              placeholder="How did you hear about me?*"
-              type="text"
-              name="source"
-              required
-            />
-            <input
-              placeholder="Your location*"
-              type="text"
-              name="location"
-              required
-            />
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+         <form className={styles.inputs} onSubmit={sendEmail}>
+            <input name="name" placeholder="Your name*" required />
+            <input name="email" type="email" placeholder="Your email*" required />
+            <input name="source" placeholder="How did you hear about me?" />
+            <input name="location" placeholder="Your location*" required />
+            <textarea name="message" placeholder="Your message"></textarea>
+
             <button type="submit">
               SUBMIT <span>></span>
             </button>
